9f4bf05b53154085cdadb553ad61cd42c76ab306
Kyle Hays <38229188+CruddyShad0w@users.noreply.github.com>
2019-04-14 18:01:52 -0700

    Update :beginner:
    - missed grab of my_etfs from context

--- algo.py
+++ algo.py
@@ -89,7 +89,7 @@ def my_pipeline(context):
     Define the pipline data columns
     '''
     # Create filter for just the ETFs we want to trade
-    universe = StaticAssets(MY_ETFS.index)
+    universe = StaticAssets(context.MY_ETFS.index)
 
     # Create any factors we need
     # latest_price is just used in case we don't have current price for an asset
